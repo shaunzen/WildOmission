c3af7c6ce41925f7f47d2977b348fe13df27f645
Larch <40319009+larch11@users.noreply.github.com>
2023-03-17 15:07:25 -0500

    Working Nametags

--- Source/WildOmission/Components/NameTagComponent.cpp
+++ Source/WildOmission/Components/NameTagComponent.cpp
@@ -2,34 +2,69 @@
 
 
 #include "NameTagComponent.h"
-#include "UObject/ConstructorHelpers.h"
+#include "GameFramework/PlayerState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 
 UNameTagComponent::UNameTagComponent()
 {
+	PrimaryComponentTick.bCanEverTick = true;
+
 	FString Placeholder = FString("Jerald :S");
 	SetText(FText::FromString(Placeholder));
 
 	// Epic thats a typo lmao
 	SetHorizontalAlignment(EHorizTextAligment::EHTA_Center);
-	
-	/*ConstructorHelpers::FObjectFinder<UFont> GameFont(TEXT("/Game/WildOmission/UI/Fonts/Akshar"));
-	if (GameFont.Object == nullptr)
-	{
-		return;
-	}
-
-	SetFont(GameFont.Object);*/
 }
 
 void UNameTagComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	// Set text to owner player name
+	
+	FString OwnerPlayerName;
+	
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if (OwnerPawn == nullptr)
+	{
+		return;
+	}
+
+	APlayerState* OwnerState = OwnerPawn->GetPlayerState();
+	if (OwnerState == nullptr)
+	{
+		return;
+	}
+
+	OwnerPlayerName = OwnerState->GetPlayerName();
+
+	SetText(FText::FromString(OwnerPlayerName));
+
+	if (GetOwner()->GetLocalRole() == ROLE_SimulatedProxy)
+	{
+		return;
+	}
+
+	SetVisibility(false);
 }
 
 void UNameTagComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// set rotation to always face player
+	// Set rotation to always face local player
+	
+	APawn* LocalPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LocalPawn == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(INDEX_NONE, 5.0f, FColor::Red, FString("failed to get local pawn"));
+		return;
+	}
+
+	FRotator TagRotation;
+	
+	TagRotation = UKismetMathLibrary::FindLookAtRotation(LocalPawn->GetActorLocation(), this->GetComponentLocation());
+	TagRotation.Yaw += 180.0f;
+	TagRotation.Pitch = 0.0f;
+
+	SetWorldRotation(TagRotation);
 }
