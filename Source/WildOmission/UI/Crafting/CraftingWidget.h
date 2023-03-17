39d594e10b23b4f3c7a8b1100c440aaa951ad67a
Larch <40319009+larch11@users.noreply.github.com>
2023-03-17 17:30:00 -0500

    created basic crafting widget

---
+++ Source/WildOmission/UI/Crafting/CraftingWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CraftingWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API UCraftingWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
