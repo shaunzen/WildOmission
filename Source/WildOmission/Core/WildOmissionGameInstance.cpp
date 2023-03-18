cdb1d53ada24a09eba2ccd1025ff990b58e26a44
Larch <40319009+larch11@users.noreply.github.com>
2023-03-18 17:27:52 -0500

    Create recipes data table

--- Source/WildOmission/Core/WildOmissionGameInstance.cpp
+++ Source/WildOmission/Core/WildOmissionGameInstance.cpp
@@ -17,7 +17,7 @@ const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
 UWildOmissionGameInstance::UWildOmissionGameInstance(const FObjectInitializer& ObjectIntializer)
 {
 	// Version Information Here
-	Version = FString("0.4.0 - Pre 2 - Prototype");
+	Version = FString("0.4.0 - Pre 3 - Prototype");
 
 	ConstructorHelpers::FClassFinder<UMainMenuWidget> MainMenuBlueprint(TEXT("/Game/WildOmission/UI/Menu/WBP_MainMenu"));
 	
