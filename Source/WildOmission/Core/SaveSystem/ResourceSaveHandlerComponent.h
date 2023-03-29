a8c6cf424041fe0835cc296baf7d972e66303bbb
Larch <40319009+larch11@users.noreply.github.com>
2023-03-29 11:57:17 -0500

    Created ResourceSaveHandlerComponent

---
+++ Source/WildOmission/Core/SaveSystem/ResourceSaveHandlerComponent.h
@@ -0,0 +1,19 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ResourceSaveHandlerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class WILDOMISSION_API UResourceSaveHandlerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UResourceSaveHandlerComponent();
+		
+};
