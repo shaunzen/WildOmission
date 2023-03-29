a14f03cac52b7bf5b02ee7982b1fabe391548c63
Larch <40319009+larch11@users.noreply.github.com>
2023-03-29 15:25:34 -0500

    wrote a very basic generation function

--- Source/WildOmission/Core/SaveSystem/ResourceSaveHandlerComponent.cpp
+++ Source/WildOmission/Core/SaveSystem/ResourceSaveHandlerComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ResourceSaveHandlerComponent.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 #include "UObject/ConstructorHelpers.h"
 
 // Sets default values for this component's properties
@@ -30,6 +31,46 @@ UResourceSaveHandlerComponent::UResourceSaveHandlerComponent()
 void UResourceSaveHandlerComponent::Generate()
 {
 	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 5.0f, FColor::Green, FString("Generating New Resources."));
+	int32 NumberOfTrees = 100;
+	int32 NumberOfStone = 100;
+	
+	for (int32 i = 0; i < NumberOfTrees; ++i)
+	{
+		bool TreeSpawnFound = false;
+		FVector LocationToSpawn;
+		int8 TypeToSpawn = FMath::RandRange(0, 2);
+
+		while (TreeSpawnFound == false)
+		{
+			TreeSpawnFound = FindSpawnLocation(LocationToSpawn);
+		}
+		//spawn it at location
+		switch(TypeToSpawn)
+		{
+		case 0:
+			GetWorld()->SpawnActor<AActor>(Tree01->StaticClass(), LocationToSpawn, FRotator::ZeroRotator);
+			break;
+		case 1:
+			GetWorld()->SpawnActor<AActor>(Tree02->StaticClass(), LocationToSpawn, FRotator::ZeroRotator);
+			break;
+		case 2:
+			GetWorld()->SpawnActor<AActor>(Tree03->StaticClass(), LocationToSpawn, FRotator::ZeroRotator);
+			break;
+		}
+	}
+
+	for (int32 i = 0; i < NumberOfStone; ++i)
+	{
+		bool FoundSpawn = false;
+		FVector LocationToSpawn;
+		
+		while (FoundSpawn == false)
+		{
+			FoundSpawn = FindSpawnLocation(LocationToSpawn);
+		}
+
+		GetWorld()->SpawnActor<AActor>(StoneNode, LocationToSpawn, FRotator::ZeroRotator);
+	}
 }
 
 void UResourceSaveHandlerComponent::Save()
@@ -40,4 +81,23 @@ void UResourceSaveHandlerComponent::Save()
 void UResourceSaveHandlerComponent::Load()
 {
 	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 5.0f, FColor::Green, FString("Loading Resources from save."));
+}
+
+bool UResourceSaveHandlerComponent::FindSpawnLocation(FVector& OutLocation)
+{
+	FHitResult HitResult;
+	FVector Start = FVector(FMath::RandRange(-10000, 10000), FMath::RandRange(-10000, 10000), 1000);
+	FVector End = Start - FVector(0, 0, 2000);
+	
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECollisionChannel::ECC_GameTraceChannel2))
+	{
+		if (HitResult.PhysMaterial == nullptr || HitResult.PhysMaterial->SurfaceType != SurfaceType1)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	return false;
 }
--- Source/WildOmission/Core/SaveSystem/ResourceSaveHandlerComponent.h
+++ Source/WildOmission/Core/SaveSystem/ResourceSaveHandlerComponent.h
@@ -24,8 +24,10 @@ public:
 private:
 	// When Biomes and more level generation related things are implemented
 	// it would make more sense to reference a data table for each biome
-	TSubclassOf<AActor*> Tree01;
-	TSubclassOf<AActor*> Tree02;
-	TSubclassOf<AActor*> Tree03;
-	TSubclassOf<AActor*> StoneNode;
+	TSubclassOf<AActor> Tree01;
+	TSubclassOf<AActor> Tree02;
+	TSubclassOf<AActor> Tree03;
+	TSubclassOf<AActor> StoneNode;
+
+	bool FindSpawnLocation(FVector& OutLocation);
 };
