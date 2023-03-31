3ebc11f64651cc7f50db4871eb79931f5861e6ca
Larch <40319009+larch11@users.noreply.github.com>
2023-03-31 09:49:12 -0500

    Item and Recipe entry for bandage

---
+++ Source/WildOmission/Items/ConsumableItem.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/EquipableItem.h"
+#include "ConsumableItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API AConsumableItem : public AEquipableItem
+{
+	GENERATED_BODY()
+	
+};
