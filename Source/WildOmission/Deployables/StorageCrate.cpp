610da502eefcffbbf59641613a514a19d1f91231
Larch <40319009+larch11@users.noreply.github.com>
2023-04-10 09:57:30 -0500

    Storage Crate class

---
+++ Source/WildOmission/Deployables/StorageCrate.cpp
@@ -0,0 +1,43 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "StorageCrate.h"
+#include "WildOmission/Components/InventoryComponent.h"
+
+// Sets default values
+AStorageCrate::AStorageCrate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mesh"));
+	RootComponent = Mesh;
+
+	InventoryComponent = CreateDefaultSubobject<UInventoryComponent>(FName("InventoryComponent"));
+
+}
+
+// Called when the game starts or when spawned
+void AStorageCrate::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStorageCrate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AStorageCrate::Interact(AActor* Interactor)
+{
+	
+}
+
+FString AStorageCrate::PromptText()
+{
+	return FString("Press 'E' to open Storage Crate");
+}
---
+++ Source/WildOmission/Deployables/StorageCrate.h
@@ -0,0 +1,40 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WildOmission/Core/Interfaces/Interactable.h"
+#include "StorageCrate.generated.h"
+
+class UInventoryComponent;
+
+UCLASS()
+class WILDOMISSION_API AStorageCrate : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AStorageCrate();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*Begin Interactable Interface Implementation*/
+	virtual void Interact(AActor* Interactor) override;
+	virtual FString PromptText() override;
+	/*End Interactable Interface Implementation*/
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+	
+	UPROPERTY(VisibleAnywhere)
+	UInventoryComponent* InventoryComponent;
+
+};
