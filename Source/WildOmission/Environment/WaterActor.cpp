1480c3bba0f78fa65e2e15999e5c219c4d30f466
Larch <40319009+larch11@users.noreply.github.com>
2023-04-17 10:41:13 -0500

    Start and stop swimming

--- Source/WildOmission/Environment/WaterActor.cpp
+++ Source/WildOmission/Environment/WaterActor.cpp
@@ -2,8 +2,7 @@
 
 
 #include "WaterActor.h"
-#include "Components/BoxComponent.h"
-#include "GameFramework/PhysicsVolume.h"
+#include "WildOmission/Characters/WildOmissionCharacter.h"
 
 // Sets default values
 AWaterActor::AWaterActor()
@@ -41,10 +40,24 @@ void AWaterActor::Tick(float DeltaTime)
 
 void AWaterActor::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	AWildOmissionCharacter* OverlappingCharacter = Cast<AWildOmissionCharacter>(OtherActor);
+	if (OverlappingCharacter == nullptr)
+	{
+		return;
+	}
+
+	OverlappingCharacter->StartSwimming();
 	UE_LOG(LogTemp, Warning, TEXT("Water Begin Overlap"));
 }
 
 void AWaterActor::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
 {
+	AWildOmissionCharacter* OverlappingCharacter = Cast<AWildOmissionCharacter>(OtherActor);
+	if (OverlappingCharacter == nullptr)
+	{
+		return;
+	}
+
+	OverlappingCharacter->StopSwimming();
 	UE_LOG(LogTemp, Warning, TEXT("Water End Overlap"));
 }
