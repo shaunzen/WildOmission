ce5cc5867fbc4acc776f7cb1371268d991d3d67e
Larch <40319009+larch11@users.noreply.github.com>
2023-04-17 10:22:09 -0500

    Water Actor Overlap events

---
+++ Source/WildOmission/Environment/WaterActor.cpp
@@ -0,0 +1,50 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "WaterActor.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/PhysicsVolume.h"
+
+// Sets default values
+AWaterActor::AWaterActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("MeshComponent"));
+	RootComponent = MeshComponent;
+	MeshComponent->SetWorldScale3D(FVector(1000.0f, 1000.0f, 50));
+	MeshComponent->SetCollisionProfileName(FName("OverlapAll"));
+
+}
+
+// Called when the game starts or when spawned
+void AWaterActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	MeshComponent->OnComponentBeginOverlap.AddDynamic(this, &AWaterActor::OnComponentBeginOverlap);
+	MeshComponent->OnComponentEndOverlap.AddDynamic(this, &AWaterActor::OnComponentEndOverlap);
+}
+
+// Called every frame
+void AWaterActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWaterActor::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Water Begin Overlap"));
+}
+
+void AWaterActor::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Water End Overlap"));
+}
---
+++ Source/WildOmission/Environment/WaterActor.h
@@ -0,0 +1,32 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WaterActor.generated.h"
+
+UCLASS()
+class WILDOMISSION_API AWaterActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWaterActor();
+	
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UFUNCTION()
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex);
+};
