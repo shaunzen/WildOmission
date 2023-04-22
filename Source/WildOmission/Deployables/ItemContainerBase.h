0a84ba023ded3cf3b1c304baff40a6dac7fd4d2d
Larch <40319009+larch11@users.noreply.github.com>
2023-04-22 11:41:00 -0500

    Game Passes initialization

---
+++ Source/WildOmission/Deployables/ItemContainerBase.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Deployable.h"
+#include "ItemContainerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API AItemContainerBase : public ADeployable
+{
+	GENERATED_BODY()
+	
+};
