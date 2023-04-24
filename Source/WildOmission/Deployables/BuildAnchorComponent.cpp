b71b6262d90247a66850d6846b404a655889b18f
Larch <40319009+larch11@users.noreply.github.com>
2023-04-24 12:07:41 -0500

    BuildAnchorComponent

---
+++ Source/WildOmission/Deployables/BuildAnchorComponent.cpp
@@ -0,0 +1,34 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "Deployables/BuildAnchorComponent.h"
+
+// Sets default values for this component's properties
+UBuildAnchorComponent::UBuildAnchorComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UBuildAnchorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UBuildAnchorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
