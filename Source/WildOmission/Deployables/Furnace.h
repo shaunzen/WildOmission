ce02724a08825696bbdd28dde2c3d7e4fd33769f
Larch <40319009+larch11@users.noreply.github.com>
2023-05-01 11:13:19 -0500

    Created Furnace class

---
+++ Source/WildOmission/Deployables/Furnace.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemContainerBase.h"
+#include "Furnace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API AFurnace : public AItemContainerBase
+{
+	GENERATED_BODY()
+	
+};
