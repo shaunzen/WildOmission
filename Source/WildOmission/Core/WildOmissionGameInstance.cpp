e4e872c8af7202276079ff18ab10184be3e15f4b
Larch <40319009+larch11@users.noreply.github.com>
2023-05-05 14:59:51 -0500

    Changed Version Number

--- Source/WildOmission/Core/WildOmissionGameInstance.cpp
+++ Source/WildOmission/Core/WildOmissionGameInstance.cpp
@@ -17,7 +17,7 @@ const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
 UWildOmissionGameInstance::UWildOmissionGameInstance(const FObjectInitializer& ObjectIntializer)
 {
 	// Version Information Here
-	Version = FString("Pre Alpha 0.6.4");
+	Version = FString("Pre Alpha 0.6.5");
 
 	ConstructorHelpers::FClassFinder<UMainMenuWidget> MainMenuBlueprint(TEXT("/Game/WildOmission/UI/Menu/WBP_MainMenu"));
 	
