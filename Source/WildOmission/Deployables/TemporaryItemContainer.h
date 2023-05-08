9c454c0d7994b2f6dbe77ea80e0bff463a0bd42a
Larch <40319009+larch11@users.noreply.github.com>
2023-05-08 11:23:23 -0500

    Temporary Item Container

---
+++ Source/WildOmission/Deployables/TemporaryItemContainer.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemContainerBase.h"
+#include "TemporaryItemContainer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API ATemporaryItemContainer : public AItemContainerBase
+{
+	GENERATED_BODY()
+	
+};
