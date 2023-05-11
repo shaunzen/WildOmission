199b8a32dc4b93cd7b46204f669a8d2cb6960dd5
Larch <40319009+larch11@users.noreply.github.com>
2023-05-11 17:31:50 -0500

    Possibly Fixed Crate Refresh Issue

--- Source/WildOmission/Components/InventoryComponent.cpp
+++ Source/WildOmission/Components/InventoryComponent.cpp
@@ -261,7 +261,7 @@ void UInventoryComponent::OnRep_ServerState()
 {
 	ClearAcknowlagedInteractions(ServerState.LastInteraction);
 	
-	if (UnacknowalgedInteractions.Num() == 0 || ServerState.LastInteraction.Time == 0)
+	if (GetOwner()->HasAuthority() || UnacknowalgedInteractions.Num() == 0 || ServerState.LastInteraction.Time == 0)
 	{
 		Slots = ServerState.Slots;
 		Contents = ServerState.Contents;
