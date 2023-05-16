4bfb3c1682685ab76068507cee266936f30545e1
Larch <40319009+larch11@users.noreply.github.com>
2023-05-16 10:16:34 -0500

    Chat Message Widget Class

--- Source/WildOmission/UI/Chat/ChatMessageWidget.cpp
+++ Source/WildOmission/UI/Chat/ChatMessageWidget.cpp
@@ -2,4 +2,11 @@
 
 
 #include "ChatMessageWidget.h"
+#include "Components/TextBlock.h"
 
+void UChatMessageWidget::Setup(const FString& PlayerName, const FString& Message, const int32& Index)
+{
+	PlayerNameText->SetText(FText::FromString(PlayerName));
+	MessageText->SetText(FText::FromString(Message));
+	MessageIndex = Index;
+}
--- Source/WildOmission/UI/Chat/ChatMessageWidget.h
+++ Source/WildOmission/UI/Chat/ChatMessageWidget.h
@@ -6,12 +6,22 @@
 #include "Blueprint/UserWidget.h"
 #include "ChatMessageWidget.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+
 UCLASS()
 class WILDOMISSION_API UChatMessageWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	void Setup(const FString& PlayerName, const FString& Message, const int32& Index);
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* PlayerNameText;
 	
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* MessageText;
+	
+	int32 MessageIndex;
+
 };
