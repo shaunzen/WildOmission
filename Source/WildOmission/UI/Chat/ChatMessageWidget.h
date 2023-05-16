6d0dcf73e8faa9d8d408ec455913b9d26ccd3c80
Larch <40319009+larch11@users.noreply.github.com>
2023-05-16 09:57:13 -0500

    Chat Widget Classes

---
+++ Source/WildOmission/UI/Chat/ChatMessageWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChatMessageWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API UChatMessageWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/WildOmission/UI/Chat/GameChatWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameChatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API UGameChatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
