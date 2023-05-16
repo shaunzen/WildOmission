7a526c855378a677f7e6cf59b91483901ce34c8a
Larch <40319009+larch11@users.noreply.github.com>
2023-05-16 10:31:57 -0500

    Game Chat Class

--- Source/WildOmission/UI/Chat/GameChatWidget.cpp
+++ Source/WildOmission/UI/Chat/GameChatWidget.cpp
@@ -2,4 +2,31 @@
 
 
 #include "GameChatWidget.h"
+#include "Components/Button.h"
+#include "UObject/ConstructorHelpers.h"
 
+UGameChatWidget::UGameChatWidget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer)
+{
+	ConstructorHelpers::FClassFinder<UChatMessageWidget> ChatMessageBlueprint(TEXT("/Game/WildOmission/UI/Chat/WBP_ChatMessage"));
+	if (ChatMessageBlueprint.Succeeded())
+	{
+		ChatMessageClass = ChatMessageBlueprint.Class;
+	}
+}
+
+void UGameChatWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SendMessageButton->OnClicked.AddDynamic(this, &UGameChatWidget::SendMessage);
+}
+
+void UGameChatWidget::RefreshMessages()
+{
+
+}
+
+void UGameChatWidget::SendMessage()
+{
+	// if no message was typed, return
+}
--- Source/WildOmission/UI/Chat/GameChatWidget.h
+++ Source/WildOmission/UI/Chat/GameChatWidget.h
@@ -6,12 +6,34 @@
 #include "Blueprint/UserWidget.h"
 #include "GameChatWidget.generated.h"
 
-/**
- * 
- */
+class UEditableTextBox;
+class UButton;
+class UChatMessageWidget;
+
 UCLASS()
 class WILDOMISSION_API UGameChatWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UGameChatWidget(const FObjectInitializer& ObjectInitializer);
+	virtual void NativeConstruct() override;
+
+	void RefreshMessages();
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	UPanelWidget* MessageContainer;
 	
+	UPROPERTY(Meta = (BindWidget))
+	UEditableTextBox* MessageBox;
+	
+	UPROPERTY(Meta = (BindWidget))
+	UButton* SendMessageButton;
+	
+	TSubclassOf<UChatMessageWidget> ChatMessageClass;
+
+	UFUNCTION()
+	void SendMessage();
+
 };
