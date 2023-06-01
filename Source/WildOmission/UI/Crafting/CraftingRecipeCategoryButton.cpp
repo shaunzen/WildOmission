ec5e746d0ea0e94ed3516e4d44717b615de6fdd7
Larch <40319009+larch11@users.noreply.github.com>
2023-06-01 10:28:12 -0500

    Text Category population

--- Source/WildOmission/UI/Crafting/CraftingRecipeCategoryButton.cpp
+++ Source/WildOmission/UI/Crafting/CraftingRecipeCategoryButton.cpp
@@ -10,10 +10,42 @@
 void UCraftingRecipeCategoryButton::Setup(UCraftingMenuWidget* InParent, TEnumAsByte<EItemCategory> InCategory)
 {
 	Button->OnClicked.AddDynamic(this, &UCraftingRecipeCategoryButton::OnClicked);
+	
 	FString ButtonTextString = FString();
-
-	// TODO text switch
-
+	switch (InCategory)
+	{
+	case All:
+		ButtonTextString = FString("All");
+		break;
+	case Item:
+		ButtonTextString = FString("Items");
+		break;
+	case Resource:
+		ButtonTextString = FString("Resource");
+		break;
+	case Tool:
+		ButtonTextString = FString("Tools");
+		break;
+	case Building:
+		ButtonTextString = FString("Building");
+		break;
+	case Weapon:
+		ButtonTextString = FString("Weapons");
+		break;
+	case Ammo:
+		ButtonTextString = FString("Ammo");
+		break;
+	case Medical:
+		ButtonTextString = FString("Medical");
+		break;
+	case Clothing:
+		ButtonTextString = FString("Clothing");
+		break;
+	default:
+		ButtonTextString = FString("Default");
+		break;
+	}
+	
 	TextBlock->SetText(FText::FromString(ButtonTextString));
 
 	ParentCraftingMenu= InParent;
