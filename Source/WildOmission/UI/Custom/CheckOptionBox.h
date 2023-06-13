a3416248d54029953ada05dcccd824444df86d28
Larch <40319009+larch11@users.noreply.github.com>
2023-06-13 16:42:48 -0500

    New Custom UI Classes

---
+++ Source/WildOmission/UI/Custom/CheckOptionBox.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CheckOptionBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API UCheckOptionBox : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/WildOmission/UI/Custom/OptionSliderBox.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OptionSliderBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSION_API UOptionSliderBox : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
