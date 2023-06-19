27de966e196e5d0c42054ace8949143388610d6f
Larch <40319009+larch11@users.noreply.github.com>
2023-06-19 11:11:59 -0500

    Created LootCrateBase

---
+++ Source/WildOmission/Deployables/LootCrateBase.cpp
@@ -0,0 +1,49 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "LootCrateBase.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "WildOmission/Components/InventoryComponent.h"
+
+ALootCrateBase::ALootCrateBase()
+{
+	GetInventoryComponent()->SetSlotCount(6);
+	ContainerName = TEXT("Loot Crate");
+}
+
+void ALootCrateBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnItems();
+}
+
+void ALootCrateBase::SpawnItems()
+{
+	if (ItemsToDrop.IsEmpty())
+	{
+		return;
+	}
+
+	int32 DropCount = FMath::RandRange(1, GetInventoryComponent()->GetSlotCount());
+	for (int32 i = 0; i < DropCount; ++i)
+	{
+		GetInventoryComponent()->AddItem(GetDrop(), GetOwner());
+	}
+}
+
+FInventoryItem ALootCrateBase::GetDrop()
+{
+	FInventoryItem ItemToDrop;
+	while (ItemToDrop.Name == FName())
+	{
+		int32 LootItemIndex =  FMath::RandRange(0, ItemsToDrop.Num() - 1);
+		bool WillDrop = UKismetMathLibrary::RandomBoolWithWeight(ItemsToDrop[LootItemIndex].Probability);
+		if (WillDrop)
+		{
+			ItemToDrop = ItemsToDrop[LootItemIndex].Item;
+		}
+	}
+
+	return ItemToDrop;
+}
