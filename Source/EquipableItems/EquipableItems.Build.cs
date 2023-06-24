1eba60c34c47da4fd42677076c11d45d1d6114be
Larch <40319009+larch11@users.noreply.github.com>
2023-06-24 10:05:46 -0500

    Created EquipableItems Module

---
+++ Source/EquipableItems/EquipableItems.Build.cs
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class EquipableItems : ModuleRules
+
+{
+
+    public EquipableItems(ReadOnlyTargetRules Target) : base(Target)
+
+    {
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "Core" });
+
+    }
+
+}
---
+++ Source/EquipableItems/EquipableItems.cpp
@@ -0,0 +1,6 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#include "EquipableItems.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FDefaultModuleImpl, EquipableItems);
