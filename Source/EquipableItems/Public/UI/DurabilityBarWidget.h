f2a3e065fb6515302ecd99361a8b6e0c871a0bce
Larch <40319009+larch11@users.noreply.github.com>
2023-07-02 10:45:32 -0500

    Created New Widgets

---
+++ Source/EquipableItems/Public/UI/DurabilityBarWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DurabilityBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EQUIPABLEITEMS_API UDurabilityBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Interaction/Public/UI/InteractionWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InteractionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTION_API UInteractionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
