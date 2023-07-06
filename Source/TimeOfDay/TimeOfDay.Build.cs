6252ff4db2e664ab74ae43602678c0f06d34b508
Larch <40319009+larch11@users.noreply.github.com>
2023-07-06 17:37:53 -0500

    Created TimeOfDay Module

---
+++ Source/TimeOfDay/TimeOfDay.Build.cs
@@ -0,0 +1,12 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TimeOfDay: ModuleRules
+{
+    public TimeOfDay(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[] { "Engine" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject" });
+    }
+}
---
+++ Source/TimeOfDay/TimeOfDayModule.cpp
@@ -0,0 +1,6 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#include "TimeOfDayModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FDefaultModuleImpl, TimeOfDay);
