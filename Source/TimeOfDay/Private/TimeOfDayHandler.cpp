cf823ff67e9804f027ba4b48139bc79506263377
Larch <dannyboirff@gmail.com>
2023-07-09 15:06:44 -0500

    Changed Icon, Fixed Day Night Bug

--- Source/TimeOfDay/Private/TimeOfDayHandler.cpp
+++ Source/TimeOfDay/Private/TimeOfDayHandler.cpp
@@ -74,20 +74,12 @@ void ATimeOfDayHandler::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (DirectionalLight == nullptr)
-	{
-		return;
-	}
-
-	// TODO setup as normal cycle time
-	DirectionalLight->AddActorLocalRotation(FRotator(DAY_NIGHT_SPEED * DeltaTime, 0.0f, 0.0f));
-	NormalizedProgressThroughDay += (DAY_NIGHT_SPEED * DeltaTime / 360.0f);
+	SetNormalizedProgressThroughDay(NormalizedProgressThroughDay + (DAY_NIGHT_SPEED * DeltaTime / 360.0f));
 	
 	if (NormalizedProgressThroughDay >= 1.0f)
 	{
-		DaysPlayed++;
-		NormalizedProgressThroughDay = 0.0f;
-		CalculateMoonPhase();
+		SetDaysPlayed(DaysPlayed + 1);
+		SetNormalizedProgressThroughDay(0.0f);
 	}
 }
 
