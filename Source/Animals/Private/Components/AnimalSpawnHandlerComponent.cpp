3ad3605bfe5eadf2f0e31371ef64fb3f5d0df80f
Larch <40319009+larch13@users.noreply.github.com>
2023-07-24 12:32:19 -0500

    Update AnimalSpawnHandlerComponent.cpp

--- Source/Animals/Private/Components/AnimalSpawnHandlerComponent.cpp
+++ Source/Animals/Private/Components/AnimalSpawnHandlerComponent.cpp
@@ -2,9 +2,12 @@
 
 
 #include "Components/AnimalSpawnHandlerComponent.h"
+#include "Engine/DataTable.h"
+#include "UObject/ConstructorHelpers.h"
 
 const static int32 MIN_SPAWN_CHECK_TIME_SECONDS = 5.0f;
 const static int32 MAX_SPAWN_CHECK_TIME_SECONDS = 15.0f;
+static UDataTable* AnimalSpawnDataTable = nullptr;
 // Need some kind of data table for referencing the diferent animals to spawn
 
 
@@ -15,7 +18,11 @@ UAnimalSpawnHandlerComponent::UAnimalSpawnHandlerComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	static ConstructorHelpers::FObjectFinder<UDataTable> AnimalDataTableObject(TEXT("/Game/Animals/DataTables/DT_AnimalSpawnData"));
+	if (AnimalDataTableObject.Succeeded())
+	{
+		AnimalSpawnDataTable = AnimalDataTableObject.Object;
+	}
 }
 
 
