d9a30970154d937cb01c6587cbc0afbe9946bed9
Larch <40319009+larch13@users.noreply.github.com>
2023-07-30 11:34:34 -0500

    Refactoring AnimInstance

--- Source/Animation/Private/WildOmissionAnimInstance.cpp
+++ Source/Animation/Private/WildOmissionAnimInstance.cpp
@@ -2,7 +2,59 @@
 
 
 #include "WildOmissionAnimInstance.h"
+#include "UObject/ConstructorHelpers.h"
+
+UWildOmissionAnimInstance::UWildOmissionAnimInstance(const FObjectInitializer& ObjectInitializer)
+{
+	Speed = 0.0f;
+	Angle = 0.0f;
+	Falling = false;
+	Swimming = false;
+
+	ConstructorHelpers::FObjectFinder<USoundBase> GrassFootstepSoundObject(TEXT("/Game/WildOmissionCore/Audio/Footsteps/Grass/HumanFootstep_Grass_Cue"));
+	ConstructorHelpers::FObjectFinder<USoundBase> GravelFootstepSoundObject(TEXT("/Game/WildOmissionCore/Audio/Footsteps/Gravel/HumanFootstep_Gravel_Cue"));
+	ConstructorHelpers::FObjectFinder<USoundBase> RockFootstepSoundObject(TEXT("/Game/WildOmissionCore/Audio/Footsteps/Rock/HumanFootstep_Rock_Cue"));
+	ConstructorHelpers::FObjectFinder<USoundBase> WoodFootstepSoundObject(TEXT("/Game/WildOmissionCore/Audio/Footsteps/Wood/HumanFootstep_Wood_Cue"));
+	ConstructorHelpers::FObjectFinder<USoundBase> WaterSplashSoundObject(TEXT("/Game/WildOmissionCore/Audio/Footsteps/Water/WaterSplash_Cue"));
+
+}
+
+void UWildOmissionAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+}
 
 void UWildOmissionAnimInstance::PlayFootstepSound()
+{
+}
+
+float UWildOmissionAnimInstance::GetSpeed() const
+{
+	return 0.0f;
+}
+
+float UWildOmissionAnimInstance::GetAngle() const
+{
+	return 0.0f;
+}
+
+bool UWildOmissionAnimInstance::IsFalling() const
+{
+	return false;
+}
+
+bool UWildOmissionAnimInstance::IsSwimming() const
+{
+	return false;
+}
+
+void UWildOmissionAnimInstance::CalculateSpeedAndAngle()
+{
+}
+
+void UWildOmissionAnimInstance::HandleFalling()
+{
+}
+
+void UWildOmissionAnimInstance::HandleSwimming()
 {
 }
--- Source/Animation/Public/WildOmissionAnimInstance.h
+++ Source/Animation/Public/WildOmissionAnimInstance.h
@@ -15,7 +15,51 @@ class ANIMATION_API UWildOmissionAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 
 public:
+	UWildOmissionAnimInstance(const FObjectInitializer& ObjectInitializer);
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
 	UFUNCTION(BlueprintCallable)
 	void PlayFootstepSound();
 	
+	UPROPERTY(BlueprintCallable)
+	float GetSpeed() const;
+
+	UPROPERTY(BlueprintCallable)
+	float GetAngle() const;
+	
+	UPROPERTY(BlueprintCallable)
+	bool IsFalling() const;
+
+	UPROPERTY(BlueprintCallable)
+	bool IsSwimming() const;
+	
+private:
+	float Speed;
+
+	float Angle;
+
+	bool Falling;
+
+	bool Swimming;
+
+	UPROPERTY()
+	USoundBase* GrassFootstepSound;
+	
+	UPROPERTY()
+	USoundBase* GravelFootstepSound;
+	
+	UPROPERTY()
+	USoundBase* RockFootstepSound;
+	
+	UPROPERTY()
+	USoundBase* WoodFootstepSound;
+	
+	UPROPERTY()
+	USoundBase* WaterSplashSound;
+
+	void CalculateSpeedAndAngle();
+	void HandleFalling();
+	void HandleSwimming();
+
 };
