3485d6d8e4cadd83a489a0771b6c848228615ba3
Larch <40319009+larch13@users.noreply.github.com>
2023-08-06 12:19:11 -0500

    Still Trying to figure out the issues

--- Source/Weather/Public/Interfaces/InteractsWithTornado.h
+++ Source/Weather/Public/Interfaces/InteractsWithTornado.h
@@ -22,6 +22,6 @@ class WEATHER_API IInteractsWithTornado
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-
 	virtual void OnContactWithTornado() = 0;
+
 };
