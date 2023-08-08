a5726e2ea95f6c2ca7e8db992d07231301ad9f9e
Larch <40319009+larch13@users.noreply.github.com>
2023-08-08 10:35:25 -0500

    Created DataTable for Save Object Lookup

---
+++ Source/SaveSystem/Private/Structs/SavableObjectDefinition.cpp
@@ -0,0 +1,9 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "Structs/SavableObjectDefinition.h"
+
+FSavableObjectDefinition::FSavableObjectDefinition()
+{
+	Class = nullptr;
+}
---
+++ Source/SaveSystem/Public/Structs/SavableObjectDefinition.h
@@ -0,0 +1,20 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "UObject/NoExportTypes.h"
+#include "SavableObjectDefinition.generated.h"
+
+USTRUCT(BlueprintType)
+struct SAVESYSTEM_API FSavableObjectDefinition : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FSavableObjectDefinition();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> Class;
+
+};
