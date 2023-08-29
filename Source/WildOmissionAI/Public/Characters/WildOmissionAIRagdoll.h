6faf1aa3351e4e9af58a4833112fc4147e65cc8b
Larch <40319009+larch13@users.noreply.github.com>
2023-08-29 15:39:09 -0500

    Create AIRagdoll Class

---
+++ Source/WildOmissionAI/Public/Characters/WildOmissionAIRagdoll.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/SkeletalMeshActor.h"
+#include "WildOmissionAIRagdoll.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDOMISSIONAI_API AWildOmissionAIRagdoll : public ASkeletalMeshActor
+{
+	GENERATED_BODY()
+	
+};
