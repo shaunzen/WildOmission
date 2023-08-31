f535165863a215d3d1708a50f963b5fc06744360
Larch <40319009+larch13@users.noreply.github.com>
2023-08-31 16:16:47 -0500

    Base Category Class

--- Source/GameSettings/Private/Interfaces/SettingsCategory.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Telephone Studios. All Rights Reserved.
-
-
-#include "Interfaces/SettingsCategory.h"
-
-// Add default functionality here for any ISettingsCategory functions that are not pure virtual.
