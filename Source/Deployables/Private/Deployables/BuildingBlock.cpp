0b69b356d5c7b06637f63ef7149b70054df83d99
Larch <40319009+larch13@users.noreply.github.com>
2023-09-05 15:02:48 -0500

    Made BuildingBlock Class

---
+++ Source/Deployables/Private/Deployables/BuildingBlock.cpp
@@ -0,0 +1,27 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+
+#include "Deployables/BuildingBlock.h"
+
+ABuildingBlock::ABuildingBlock()
+{
+	UpgradedTier = nullptr;
+}
+
+void ABuildingBlock::Upgrade()
+{
+	if (UpgradedTier == nullptr)
+	{
+		return;
+	}
+
+	ABuildingBlock* SpawnedBuildingBlock = GetWorld()->SpawnActor<ABuildingBlock>(UpgradedTier, this->GetActorLocation(), this->GetActorRotation());
+	if (SpawnedBuildingBlock == nullptr)
+	{
+		return;
+	}
+
+	SpawnedBuildingBlock->OnSpawn();
+
+	this->Destroy();
+}
---
+++ Source/Deployables/Public/Deployables/BuildingBlock.h
@@ -0,0 +1,24 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Deployables/Deployable.h"
+#include "BuildingBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEPLOYABLES_API ABuildingBlock : public ADeployable
+{
+	GENERATED_BODY()
+	
+public:
+	ABuildingBlock();
+	void Upgrade();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Deployable")
+	TSubclassOf<ABuildingBlock> UpgradedTier;
+};
