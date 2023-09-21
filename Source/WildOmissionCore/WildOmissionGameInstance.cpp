dbced1050364ce32827e5b76e150403d64ce99a4
Larch <40319009+larch13@users.noreply.github.com>
2023-09-21 11:59:38 -0500

    Updated Version Number

--- Source/WildOmissionCore/WildOmissionGameInstance.cpp
+++ Source/WildOmissionCore/WildOmissionGameInstance.cpp
@@ -22,7 +22,7 @@ const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
 const static FName FRIENDS_ONLY_SETTINGS_KEY = TEXT("FriendsOnlySession");
 const static FName LEVEL_FILE_SETTINGS_KEY = TEXT("LevelFile");
 const static FName SEARCH_PRESENCE = TEXT("PRESENCESEARCH");
-const static FString GameVersion = TEXT("Pre Alpha 0.9.7");
+const static FString GameVersion = TEXT("Pre Alpha 0.9.7_01");
 
 static USoundMix* MasterSoundMixModifier = nullptr;
 static USoundClass* MasterSoundClass = nullptr;
