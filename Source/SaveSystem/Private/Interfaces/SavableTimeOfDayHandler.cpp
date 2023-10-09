e01428f271bfa5bee8d7ad9212dec57b6d7cc0d5
Larch <40319009+larch13@users.noreply.github.com>
2023-10-09 15:21:50 -0500

    Removed Old SaveSystem Interfaces

--- Source/SaveSystem/Private/Interfaces/SavableTimeOfDayHandler.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Telephone Studios. All Rights Reserved.
-
-
-#include "Interfaces/SavableTimeOfDayHandler.h"
-
-// Add default functionality here for any ISavableTimeOfDayHandler functions that are not pure virtual.
--- Source/SaveSystem/Private/Interfaces/SavableWeatherHandler.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Telephone Studios. All Rights Reserved.
-
-
-#include "Interfaces/SavableWeatherHandler.h"
-
-// Add default functionality here for any ISavableWeatherHandler functions that are not pure virtual.
