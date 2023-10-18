8ef23a68485b36ac934ed6a14c2917b6a545d32d
Larch <40319009+larch13@users.noreply.github.com>
2023-10-18 12:07:19 -0500

    Added Torch

---
+++ Source/EquipableItems/Public/Items/Torch.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/EquipableItem.h"
+#include "Torch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EQUIPABLEITEMS_API ATorch : public AEquipableItem
+{
+	GENERATED_BODY()
+	
+};
