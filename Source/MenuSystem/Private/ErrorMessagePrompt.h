eeac5b0450c0d10701aa20d2f4e5ae2ddb26a2bf
Larch <40319009+larch13@users.noreply.github.com>
2023-11-01 11:35:07 -0500

    Functions For Setting Error Messages

--- Source/MenuSystem/Private/ErrorMessagePrompt.h
+++ Source/MenuSystem/Private/ErrorMessagePrompt.h
@@ -6,12 +6,37 @@
 #include "Blueprint/UserWidget.h"
 #include "ErrorMessagePrompt.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class UButton;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCloseButtonClickedSignature);
+
 UCLASS()
 class UErrorMessagePrompt : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UErrorMessagePrompt(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable)
+	void SetMessage(const FString& MessageTitle, const FString& Message);
+
+	FOnCloseButtonClickedSignature OnCloseButtonClicked;
+
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* TitleTextBlock;
+
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* MessageTextBlock;
+
+	UPROPERTY(Meta = (BindWidget))
+	UButton* CloseButton;
+
+	UFUNCTION()
+	void BroadcastCloseButtonClicked();
+
 };
