b0f92dbd49db3464c00b7a3b4bf709be37d5f7ae
Larch <40319009+larch13@users.noreply.github.com>
2023-11-01 10:28:50 -0500

    Created ErrorMessagePrompt Class

---
+++ Source/MenuSystem/Private/ErrorMessagePrompt.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ErrorMessagePrompt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UErrorMessagePrompt : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
