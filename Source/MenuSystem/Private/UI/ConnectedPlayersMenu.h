80f7d1f3677965e5de5234eccd997d90abe10727
Larch <40319009+larch13@users.noreply.github.com>
2023-11-03 15:05:20 -0500

    Created ConnectedPlayersMenu class

---
+++ Source/MenuSystem/Private/UI/ConnectedPlayersMenu.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ConnectedPlayersMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UConnectedPlayersMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
