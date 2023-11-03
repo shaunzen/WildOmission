8af228574f5bf8a30e5596f7d3ed2519bc3ccb88
Larch <40319009+larch13@users.noreply.github.com>
2023-11-03 16:47:43 -0500

    Player Row Widget Class

---
+++ Source/MenuSystem/Private/UI/PlayerRowWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerRowWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPlayerRowWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
