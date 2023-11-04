c15ab4213ae68faf151f4e3fe1931bcec132d04d
Larch <40319009+larch13@users.noreply.github.com>
2023-11-04 10:20:45 -0500

    PlayerRow Widget

--- Source/MenuSystem/Private/UI/PlayerRowWidget.cpp
+++ Source/MenuSystem/Private/UI/PlayerRowWidget.cpp
@@ -2,4 +2,47 @@
 
 
 #include "UI/PlayerRowWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+UPlayerRowWidget::UPlayerRowWidget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer)
+{
+	NameTextBlock = nullptr;
+	ViewProfileButton = nullptr;
+	KickButton = nullptr;
+	UniqueID = TEXT("");
+}
+
+void UPlayerRowWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ViewProfileButton)
+	{
+		ViewProfileButton->OnClicked.AddDynamic(this, &UPlayerRowWidget::ViewProfile);
+	}
+
+	if (KickButton)
+	{
+		KickButton->OnClicked.AddDynamic(this, &UPlayerRowWidget::Kick);
+	}
+}
+
+void UPlayerRowWidget::Setup(const FString& Name, const FString& InUniqueID)
+{
+	NameTextBlock->SetText(FText::FromString(Name));
+	UniqueID = InUniqueID;
+}
+
+void UPlayerRowWidget::ViewProfile()
+{
+	const FString ProfileURL = FString::Printf(TEXT("https://steamcommunity.com/id/%s/"), *UniqueID);
+	UKismetSystemLibrary::LaunchURL(ProfileURL);
+}
+
+void UPlayerRowWidget::Kick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Kicking player...\nKick Functionality isnt implemented yet."));
+	// TODO figure this out later
+}
--- Source/MenuSystem/Private/UI/PlayerRowWidget.h
+++ Source/MenuSystem/Private/UI/PlayerRowWidget.h
@@ -6,12 +6,38 @@
 #include "Blueprint/UserWidget.h"
 #include "PlayerRowWidget.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class UButton;
+
 UCLASS()
 class UPlayerRowWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPlayerRowWidget(const FObjectInitializer& ObjectInitializer);
+
+	virtual void NativeConstruct() override;
+
+	void Setup(const FString& Name, const FString& InUniqueID);
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* NameTextBlock;
+
+	UPROPERTY(Meta = (BindWidget))
+	UButton* ViewProfileButton;
+
+	UPROPERTY(Meta = (BindWidget))
+	UButton* KickButton;
+
+	UPROPERTY()
+	FString UniqueID;
+
+	UFUNCTION()
+	void ViewProfile();
+
+	UFUNCTION()
+	void Kick();
+
 };
