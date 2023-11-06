7e6fc4bdf6fc011fe73af4089ad7a27a9e77a384
Larch <40319009+larch13@users.noreply.github.com>
2023-11-06 15:47:26 -0600

    Create KeypadWidget class

---
+++ Source/Deployables/Private/UI/KeypadWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "KeypadWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UKeypadWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
