ff7d3290f7643fd98eef42b72503efcc9c9d10cd
Larch <40319009+larch13@users.noreply.github.com>
2023-11-06 15:54:23 -0600

    Filled in KeypadWidget class

--- Source/Deployables/Private/UI/KeypadWidget.cpp
+++ Source/Deployables/Private/UI/KeypadWidget.cpp
@@ -3,3 +3,20 @@
 
 #include "UI/KeypadWidget.h"
 
+UKeypadWidget::UKeypadWidget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer)
+{
+	CodeTextBlock = nullptr;
+	OneKeyButton = nullptr;
+	TwoKeyButton = nullptr;
+	ThreeKeyButton = nullptr;
+	FourKeyButton = nullptr;
+	FiveKeyButton = nullptr;
+	SixKeyButton = nullptr;
+	SevenKeyButton = nullptr;
+	EightKeyButton = nullptr;
+	NineKeyButton = nullptr;
+	ZeroKeyButton = nullptr;
+	ToggleLockButton = nullptr;
+	ToggleLockTextBlock = nullptr;
+	CloseButton = nullptr;
+}
--- Source/Deployables/Private/UI/KeypadWidget.h
+++ Source/Deployables/Private/UI/KeypadWidget.h
@@ -6,12 +6,45 @@
 #include "Blueprint/UserWidget.h"
 #include "KeypadWidget.generated.h"
 
-/**
- * 
- */
+class UTextBlock;
+class UButton;
+
 UCLASS()
 class UKeypadWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UKeypadWidget(const FObjectInitializer& ObjectInitializer);
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* CodeTextBlock;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* OneKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* TwoKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* ThreeKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* FourKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* FiveKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* SixKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* SevenKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* EightKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* NineKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* ZeroKeyButton;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* ToggleLockButton;
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* ToggleLockTextBlock;
+	UPROPERTY(Meta = (BindWidget))
+	UButton* CloseButton;
+
 };
