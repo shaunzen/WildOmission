0fe2b44a30a2302af68b2cde58283002449ba7c5
Larch <40319009+larch13@users.noreply.github.com>
2023-11-11 14:54:56 -0600

    Started working on tool cupboard widget

---
+++ Source/Deployables/Private/UI/ToolCupboardWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ToolCupboardWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UToolCupboardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
