710caa67de874a145c9d6188ed508f5eddff73bb
Larch <40319009+larch13@users.noreply.github.com>
2023-11-13 11:16:40 -0600

    Created New Widget Classes

---
+++ Source/MenuSystem/Private/UI/DeleteWorldWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DeleteWorldWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDeleteWorldWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MenuSystem/Private/UI/RenameWorldWidget.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RenameWorldWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class URenameWorldWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
