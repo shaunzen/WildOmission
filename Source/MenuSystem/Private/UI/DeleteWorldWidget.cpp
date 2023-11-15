fa785522162277ca334a4bce9aee90bed4d3a4c9
Larch <40319009+larch13@users.noreply.github.com>
2023-11-15 09:49:38 -0600

    Created Delegates for delete world widget

--- Source/MenuSystem/Private/UI/DeleteWorldWidget.cpp
+++ Source/MenuSystem/Private/UI/DeleteWorldWidget.cpp
@@ -2,4 +2,37 @@
 
 
 #include "UI/DeleteWorldWidget.h"
+#include "Components/Button.h"
 
+UDeleteWorldWidget::UDeleteWorldWidget(const FObjectInitializer& ObjectInitializer) : UUserWidget(ObjectInitializer)
+{
+	DeleteButton = nullptr;
+	CancelButton = nullptr;
+}
+
+void UDeleteWorldWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	DeleteButton->OnClicked.AddDynamic(this, &UDeleteWorldWidget::BroadcastDeleteButtonClicked);
+	CancelButton->OnClicked.AddDynamic(this, &UDeleteWorldWidget::BroadcastCancelButtonClicked);
+}
+
+void UDeleteWorldWidget::BroadcastDeleteButtonClicked()
+{
+	if (!OnDeleteButtonClicked.IsBound())
+	{
+		return;
+	}
+
+	OnDeleteButtonClicked.Broadcast();
+}
+
+void UDeleteWorldWidget::BroadcastCancelButtonClicked()
+{
+	if (!OnCancelButtonClicked.IsBound())
+	{
+		return;
+	}
+
+	OnCancelButtonClicked.Broadcast();
+}
--- Source/MenuSystem/Private/UI/DeleteWorldWidget.h
+++ Source/MenuSystem/Private/UI/DeleteWorldWidget.h
@@ -6,12 +6,33 @@
 #include "Blueprint/UserWidget.h"
 #include "DeleteWorldWidget.generated.h"
 
-/**
- * 
- */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDeleteWorldOnDeleteButtonClickedSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDeleteWorldOnCancelButtonClickedSignature);
+
 UCLASS()
 class UDeleteWorldWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UDeleteWorldWidget(const FObjectInitializer& ObjectInitializer);
+
+	virtual void NativeConstruct() override;
+	
+	FDeleteWorldOnDeleteButtonClickedSignature OnDeleteButtonClicked;
+	FDeleteWorldOnCancelButtonClickedSignature OnCancelButtonClicked;
+
+private:
+	UPROPERTY(Meta = (BindWidget))
+	class UButton* DeleteButton;
+
+	UPROPERTY(Meta = (BindWidget))
+	class UButton* CancelButton;
+
+	UFUNCTION()
+	void BroadcastDeleteButtonClicked();
+
+	UFUNCTION()
+	void BroadcastCancelButtonClicked();
+
 };
