f1616b85b7a458618f5d50fcb78da8a820ed0bc7
Larch <40319009+larch13@users.noreply.github.com>
2023-12-29 16:17:33 -0600

    Lowered Render Distance

--- Source/WorldGeneration/Private/ChunkManager.cpp
+++ Source/WorldGeneration/Private/ChunkManager.cpp
@@ -10,7 +10,7 @@
 static AChunkManager* Instance = nullptr;
 static UDataTable* BiomeGenerationDataTable = nullptr;
 
-const static int32 RENDER_DISTANCE = 32;
+const static int32 RENDER_DISTANCE = 16;
 
 // Sets default values
 AChunkManager::AChunkManager()
