9cdd74106c26d7d95d27932e004d68320924158a
Larch <40319009+larch13@users.noreply.github.com>
2024-01-16 10:10:59 -0600

    Basic map item that brings up widget

--- Source/Maps/Public/Items/MapItem.h
+++ Source/Maps/Public/Items/MapItem.h
@@ -14,4 +14,19 @@ class MAPS_API AMapItem : public AEquipableItem
 {
 	GENERATED_BODY()
 	
+public:
+	AMapItem();
+
+	virtual void OnPrimaryPressed() override;
+	virtual void OnSecondaryPressed() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<class UMapWidget> MapWidgetClass;
+
+	UPROPERTY()
+	class UMapWidget* OpenWidget;
+
+	void BringUpMap();
+
 };
