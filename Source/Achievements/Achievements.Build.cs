9d752054c4824b071e0022da8046cf6bc1f7b528
Larch <40319009+larch13@users.noreply.github.com>
2024-01-27 10:37:04 -0600

    Created AchievementManager class

--- Source/Achievements/Achievements.Build.cs
+++ Source/Achievements/Achievements.Build.cs
@@ -7,6 +7,6 @@ public class Achievements : ModuleRules
     public Achievements(ReadOnlyTargetRules Target) : base(Target)
     {
         PublicDependencyModuleNames.AddRange(new string[] { "Engine", "UMG" });
-        PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "OnlineSubsystem" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "OnlineSubsystem", "OnlineSubsystemSteam" });
     }
 }
---
+++ Source/Achievements/Public/AchievementManager.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AchievementManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACHIEVEMENTS_API UAchievementManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
