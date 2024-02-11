d0e59d7fbd6b5ec69f55762ed98237883b8320c7
Larch <40319009+larch13@users.noreply.github.com>
2024-02-11 12:28:50 -0600

    Created Sign class

---
+++ Source/Deployables/Public/Deployables/Sign.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Deployables/Deployable.h"
+#include "Sign.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEPLOYABLES_API ASign : public ADeployable
+{
+	GENERATED_BODY()
+	
+};
