13872048c6bd1d072bf93e69ee7d043c78d870a6
Larch <40319009+larch13@users.noreply.github.com>
2024-02-11 15:49:18 -0600

    Laying out sign class

--- Source/Deployables/Private/Deployables/Sign.cpp
+++ Source/Deployables/Private/Deployables/Sign.cpp
@@ -2,8 +2,25 @@
 
 
 #include "Deployables/Sign.h"
-#include ""
+#include "Components/TextRenderComponent.h"
+
 ASign::ASign()
 {
+	TextRenderComponent = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TextRenderComponent"));
+}
 
+void ASign::Interact(AActor* Interactor)
+{
+	Super::Interact(Interactor);
+
+	// TODO only allow to bring up if we have building priv
+	// TODO bring up control menu
+
+}
+
+FString ASign::PromptText()
+{
+	Super::PromptText();
+
+	return TEXT("");
 }
--- Source/Deployables/Public/Deployables/Sign.h
+++ Source/Deployables/Public/Deployables/Sign.h
@@ -4,18 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "Deployables/Deployable.h"
+#include "Interfaces/Interactable.h"
 #include "Sign.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class DEPLOYABLES_API ASign : public ADeployable
+class DEPLOYABLES_API ASign : public ADeployable, public IInteractable
 {
 	GENERATED_BODY()
 	
 public:
 	ASign();
+
+	// Begin IInteractable Implementation
+	virtual void Interact(AActor* Interactor) override;
+	virtual FString PromptText() override;
+	// End IInteractable Implementation
 
 private:
 	UPROPERTY(VisibleAnywhere)
