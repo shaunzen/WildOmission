9624fcc36991d6cd6d915a41d266ee1336cc1919
Larch <40319009+larch13@users.noreply.github.com>
2024-02-13 01:35:49 -0600

    Monster attack damage difficulty multiplier

--- Source/Monsters/Private/Items/MonsterArmsItem.cpp
+++ Source/Monsters/Private/Items/MonsterArmsItem.cpp
@@ -3,12 +3,36 @@
 
 #include "Items/MonsterArmsItem.h"
 #include "Monsters/Monster.h"
+#include "SaveManager.h"
+#include "WildOmissionSaveGame.h"
 #include "Components/EquipComponent.h"
 #include "Engine/DamageEvents.h"
 
 AMonsterArmsItem::AMonsterArmsItem()
 {
 	DamageMultiplier = 0.3f;
+}
+
+void AMonsterArmsItem::Equip(APawn* InOwnerPawn, USkeletalMeshComponent* InThirdPersonMeshComponent, const FName& InItemName, const int8& InFromSlotIndex, const uint32& InUniqueID)
+{
+	Super::Equip(InOwnerPawn, InThirdPersonMeshComponent, InItemName, InFromSlotIndex, InUniqueID);
+
+	// Set damage multiplayer depending on difficulty
+	ASaveManager* SaveManager = ASaveManager::GetSaveManager();
+	if (SaveManager == nullptr)
+	{
+		return;
+	}
+
+	UWildOmissionSaveGame* SaveFile = SaveManager->GetSaveFile();
+	if (SaveFile == nullptr)
+	{
+		return;
+	}
+
+	// 0 = peace, 1 = easy, 2 = normal, 3 = hard
+	const int8 DifficultyModifier = (SaveFile->Difficulty.GetIntValue() - 2) * 0.15f;
+	DamageMultiplier = FMath::Clamp(DamageMultiplier * DifficultyModifier, 0.1f, 1.0f);
 }
 
 void AMonsterArmsItem::OnPrimaryHeld()
