c4caf39e9a4d45ec18137bf76f887ff7870790e1
Larch <40319009+larch13@users.noreply.github.com>
2024-05-30 15:18:41 -0500

    Basic Rocket Stuff

---
+++ Source/Weapons/Public/Projectiles/RocketProjectile.h
@@ -0,0 +1,17 @@
+// Copyright Telephone Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectiles/WeaponProjectile.h"
+#include "RocketProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEAPONS_API ARocketProjectile : public AWeaponProjectile
+{
+	GENERATED_BODY()
+	
+};
