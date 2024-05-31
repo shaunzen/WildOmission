89e21413ad6cccf41909b802700d0a3549d6132d
Larch <40319009+larch13@users.noreply.github.com>
2024-05-31 11:25:17 -0500

    Rockets Working

--- Source/Weapons/Private/Projectiles/RocketProjectile.cpp
+++ Source/Weapons/Private/Projectiles/RocketProjectile.cpp
@@ -2,21 +2,70 @@
 
 
 #include "Projectiles/RocketProjectile.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Engine/DamageEvents.h"
+#include "Kismet/GameplayStatics.h"
 
 ARocketProjectile::ARocketProjectile()
 {
-
+    ExplosionSound = nullptr;
+    ExplosionSystem = nullptr;
 }
 
+// On hit is only called on the server, effects like sound and particles are triggered through RPC calls
+// Explosion damage is handled here
 void ARocketProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
+    // Just a quick check to make sure we arent getting used incorrectly
+    if (!HasAuthority())
+    {
+        Super::OnHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
+        return;
+    }
+
+    // we will do an overlap test
+    UWorld* World = GetWorld();
+    if (World == nullptr)
+    {
+        return;
+    }
+
+    // whatever overlaps, we will do a damage event, and use our distance to calculate the damage
+    TArray<FOverlapResult> Overlaps;
+    FCollisionShape Shape = FCollisionShape::MakeSphere(400);
+    World->OverlapMultiByChannel(Overlaps, this->GetActorLocation(), this->GetActorRotation().Quaternion(), ECollisionChannel::ECC_Visibility, Shape);
+    if (Overlaps.IsEmpty())
+    {
+        return;
+    }
+
+    for (FOverlapResult Overlap : Overlaps)
+    {
+        AActor* OverlapedActor = Overlap.GetActor();
+        if (OverlapedActor == nullptr)
+        {
+            continue;
+        }
+
+        if (Cast<APawn>(OverlapedActor))
+        {
+            PlayHitmarkerSound(false);
+        }
+        
+        float DamageAmount = 50.0f;
+        FRadialDamageEvent RadialDamage;
+        OverlapedActor->TakeDamage(DamageAmount, RadialDamage, Cast<AController>(this->GetOwner()), this);
+    }
+
+    Multi_PlayExplosionEffects();
+
     Super::OnHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
+}
 
 
-    UE_LOG(LogTemp, Warning, TEXT("OnHit Called on server = %i"), HasAuthority());
-    // Is this on the server?
-    
-    // TODO do explosion
-
-    // TODO play boom sound
+void ARocketProjectile::Multi_PlayExplosionEffects_Implementation()
+{
+    UGameplayStatics::PlaySoundAtLocation(GetWorld(), ExplosionSound, this->GetActorLocation());
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ExplosionSystem, this->GetActorLocation());
 }
--- Source/Weapons/Public/Projectiles/RocketProjectile.h
+++ Source/Weapons/Public/Projectiles/RocketProjectile.h
@@ -15,12 +15,19 @@ class WEAPONS_API ARocketProjectile : public AWeaponProjectile
 	GENERATED_BODY()
 	
 public:
-
 	ARocketProjectile();
 
 protected:
-
 	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
 
+private:
+	UPROPERTY(EditDefaultsOnly)
+	USoundBase* ExplosionSound;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UNiagaraSystem* ExplosionSystem;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multi_PlayExplosionEffects();
 
 };
