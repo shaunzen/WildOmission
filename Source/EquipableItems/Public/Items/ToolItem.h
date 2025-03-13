ae00f094b2e50866b6110ad00091cb2d76504891
Larch <40319009+larch13@users.noreply.github.com>
2025-03-13 10:07:18 -0500

    Added Documentation to ToolItem class

--- Source/EquipableItems/Public/Items/ToolItem.h
+++ Source/EquipableItems/Public/Items/ToolItem.h
@@ -23,12 +23,27 @@ public:
 	bool IsQualityTool() const;
 
 protected:
+	/*
+	Used to determine which harvestables this tool can gather from.
+	*/
 	UPROPERTY(EditDefaultsOnly, Category = "Item")
 	TEnumAsByte<EToolType> ToolType;
 	
+	/*
+	Used as a decimal the it is multipled by the output of a harvestable resource,
+	and that multiplied amount is given to the player.
+	The base Value varies from harvestable to harvestable but is generally 100.
+	The stone hatchet has a gather multiplier of 0.2, making it give 20 wood from a tree
+	each hit.
+	*/
 	UPROPERTY(EditDefaultsOnly, Category = "Item")
 	float GatherMultiplier;
 
+	/* 
+	Used by harvestable resources to determine if it should drop extra goodies reserved for the nicer tools 
+	An example would be that metal tools can harvest Refined Metal when hitting metal nodes, because they are a "QualityTool" whereas
+	the Stone Pickaxe cannot
+	*/
 	UPROPERTY(EditDefaultsOnly, Category = "Item")
 	bool QualityTool;
 
--- Source/GatherableResources/Public/Enums/ToolType.h
+++ Source/GatherableResources/Public/Enums/ToolType.h
@@ -9,11 +9,19 @@
 UENUM()
 enum EToolType
 {
+	// Wood is for trees.
 	WOOD	UMETA(DisplayName = "Wood"),
+	// Stone is for nodes.
 	STONE	UMETA(DisplayName = "Stone"),
+	// Unused as far as I know.
 	METAL	UMETA(DisplayName = "Metal"),
+	// Multi is used by the rock to allow it to gather both node and wood resources.
 	MULTI	UMETA(DisplayName = "Multi-Tool"),
+	// Unassigned / Generic.
 	NONE	UMETA(DisplayName = "None"),
+	// Used to denote that it's for building purposes only
+	// The building hammer uses this one.
 	BUILD	UMETA(DisplayName = "Building Tool"),
+	// Unused :3
 	FABRIC	UMETA(DisplayName = "Fabric")
 };
